eea00b8aca2be30049130de10a2745ea0248cdd7
Mason Ellwood <masonellwood@gmail.com>
2020-05-19 23:09:59 -0700

    inital route added

---
+++ lib/pages/single_author_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleAuthorPage extends StatefulWidget {
+  @override
+  _SingleAuthorPageState createState() => _SingleAuthorPageState();
+}
+
+class _SingleAuthorPageState extends State<SingleAuthorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('flutter'),
+    );
+  }
+}
